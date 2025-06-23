16946cc1f582c68add43ca994fd22be41d6a4547
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-06-23 07:19:10 +0300

    BurgerButton created

---
+++ src/components/ui/BurgerButton/BurgerButton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { CiMenuBurger } from "react-icons/ci"
+/**
+ * Компонент кнопки бургера для мобильного меню.
+ *
+ * Этот компонент отображает кнопку с иконкой бургера и текстом "Меню".
+ * Кнопка скрыта на больших экранах и отображается на мобильных устройствах.
+ *
+ * @component
+ * @param {Object} props - Свойства компонента.
+ * @param {function} props.onClick - Функция, вызываемая при клике на кнопку.
+ * @returns {JSX.Element} Элемент кнопки бургера.
+ *
+ * @example
+ * const handleMenuToggle = () => {
+ *     console.log("Меню открыто!");
+ * };
+ *
+ * return <BurgerButton onClick={handleMenuToggle} />;
+ */
+type BurgerButtonProps = {
+    onClick:()=>void
+}
+const BurgerButton:React.FC<BurgerButtonProps> = ({ onClick }) => {
+
+  return (
+
+    <button className="burger-button" onClick={onClick}>
+      <CiMenuBurger />
+      <p>Меню</p>
+    </button>
+  );
+};
+
+export default BurgerButton;
