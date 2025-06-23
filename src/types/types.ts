a3617951f75869aac7157685206492d86818dd68
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-06-23 07:33:00 +0300

    types created

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+// types.ts
+export interface ProductItem {
+  id: string;
+  name: string;
+  category: string;
+  color: string | string[];
+  price: string;
+  imgSrc: string;
+  quantity: number;
+}
