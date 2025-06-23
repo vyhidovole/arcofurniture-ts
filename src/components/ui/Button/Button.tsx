c6f95006f5e4031e393b5ae7e6bcb47ac7d4080a
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-06-23 07:20:18 +0300

    Button created

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { BiLoaderAlt } from "react-icons/bi";
+import styles from "./Button.module.css";
+
+type ButtonProps = {
+    className?: string
+    onClick?: ()=> void
+    variant?:"primary"|"secondary"|"nagative"|"ghost"|"link"
+    type?:"submit"|"button"|"reset"
+    suffix?: React.ReactNode
+    icon?: React.ReactNode
+    isLoading?: boolean
+    children?: React.ReactNode
+    disabled?: boolean
+}
+/**
+ * Компонент кнопки
+ */
+const Button: React.FC<ButtonProps>=({
+    className = "",
+    onClick,
+    variant = "primary",
+    type = "submit",
+    suffix,
+    icon,
+    isLoading = false,
+    children,
+    disabled = false,
+}) => {
+    //Формируем класс  с учетом стилей из css Modules
+    const variantClassName = styles[variant]||""
+    const combinedClassName = [
+        "my-button",
+        variantClassName,
+        className,
+    ].filter(Boolean).join(" ")//альтернатива clsx и classnames
+
+    return (
+        <button className={combinedClassName}
+        disabled={disabled||isLoading}
+        onClick={onClick}
+        type = {type}
+        >
+            {isLoading?(
+                <BiLoaderAlt className="biLoaderAlt"/>
+            ):(
+                <>
+                {icon}
+                {children}
+                {suffix && <span className="ml-2">{suffix}</span>}
+                
+                </>
+            )}
+
+        </button>
+    )
+
+}
+export default Button;
