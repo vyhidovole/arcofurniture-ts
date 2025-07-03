efaa1245422e113ac5823289b4956ccc9b061a60
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-07-03 10:16:32 +0300

    fixed

--- src/pages/drawingroom .tsx
+++ src/pages/drawingroom .tsx
@@ -1,13 +1,13 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
 import Image from "next/image";
-import { useLoading } from '@/context/LoadingContext'; 
+import { useLoading } from '@/context/LoadingContext';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import catalogueStore from "@/store/CatalogueStore"; 
+import catalogueStore from "@/store/CatalogueStore";
 import Alert from "@/components/ui/Alert/Alert";
-import { useCart } from '@/context/CartContext'; 
-import {ProductItem} from '@/types/types'
+import { useCart } from '@/context/CartContext';
+import { ProductItem } from '@/types/types'
 import styles from './drawingroom.module.css'
 
 /**
@@ -21,23 +21,23 @@ import styles from './drawingroom.module.css'
  * return <Drawing_room />;
  */
 
-const Drawing_room = observer(() => { 
+const DrawingRoom = observer(() => {
   const { loading, setLoading } = useLoading(); // Получаем состояние загрузки
-   const {  addToCart } = useCart(); // Используем контекст
+  const { addToCart } = useCart(); // Используем контекст
   // Стейт для закрытия компонента уведомления
   const [isShowAlert, setShowAlert] = useState(false);
   useEffect(() => {
     console.log("isShowAlert изменился на:", isShowAlert);
-}, [isShowAlert]);
+  }, [isShowAlert]);
   // const { products, basket } = catalogueStore; // Получаем продукты и корзину из store
-  
-   /**
-   * Функция для добавления товара в корзину.
-   * Добавляет продукт в корзину и отображает уведомление.
-   *
-   * @param {Object} item - Объект товара, который нужно добавить в корзину.
-   */
-  const handleAddToBasket = (item:ProductItem) => {
+
+  /**
+  * Функция для добавления товара в корзину.
+  * Добавляет продукт в корзину и отображает уведомление.
+  *
+  * @param {Object} item - Объект товара, который нужно добавить в корзину.
+  */
+  const handleAddToBasket = (item: ProductItem) => {
     catalogueStore.addProductToBasket(item); // Добавляем продукт в корзину
     addToCart()
     console.log(`${item.name} добавлен в корзину!`);
@@ -45,16 +45,16 @@ const Drawing_room = observer(() => {
     console.log("Показать алерт:", true); // Логируем изменение состояния
     setTimeout(() => {
       setShowAlert(false); // Скрываем алерт
-  }, 3000); 
-};
+    }, 3000);
+  };
   useEffect(() => {
-    const url = '/drawingroom'; // Определяем конечный URL 
+    const url = '/drowingroom'; // Определяем конечный URL 
     setLoading(true); // Устанавливаем состояние загрузки в true
     catalogueStore.getProducts(url).finally(() => {
-         setLoading(false); // Устанавливаем состояние загрузки в false после завершения запроса
-       });
-     }, [setLoading]);
- 
+      setLoading(false); // Устанавливаем состояние загрузки в false после завершения запроса
+    });
+  }, [setLoading]);
+
   // Итерация по данным и отрисовка карточек
   const renderData =
     catalogueStore.products.length > 0 &&
@@ -63,6 +63,8 @@ const Drawing_room = observer(() => {
         <Image
           src={item.imgSrc || '/path/to/default-image.jpg'}
           alt={item.name}
+          width={250}
+          height={300}
           className="drawing-img"
         />
         <h2>{item.name || 'Нет имени'}</h2>
@@ -94,30 +96,30 @@ const Drawing_room = observer(() => {
         <p>{item.price || 'Нет цены'}</p>
         <button
           className="drawing-button "
-           onClick={() => handleAddToBasket(item)} >
+          onClick={() => handleAddToBasket(item)} >
           купить
         </button>
       </div>
     ));
-    console.log("Текущая корзина:", catalogueStore.basket);
-    console.log(catalogueStore.products); // Логируем продукты для отладки
-    console.log("Количество уникальных товаров в корзине:", catalogueStore.quantity); // Логируем количество уникальных товаров
+  console.log("Текущая корзина:", catalogueStore.basket);
+  console.log(catalogueStore.products); // Логируем продукты для отладки
+  console.log("Количество уникальных товаров в корзине:", catalogueStore.quantity); // Логируем количество уникальных товаров
 
-    return (
-      <>
-       <div className={styles["grid-container"]}>
-       {loading ? (
+  return (
+    <>
+      <div className={styles["grid-container"]}>
+        {loading ? (
           // Отображение Skeleton, пока данные загружаются
           Array.from({ length: 8 }).map((_, index) => (
-     <div key={index} className={styles['small-container']}>
-      <Skeleton height="100%" />
+            <div key={index} className={styles['small-container']}>
+              <Skeleton height="100%" />
+            </div>
+          ))
+        ) : (
+          renderData
+        )}
       </div>
-    ))
-  ) :(
-    renderData
-  )}
-     </div>   
-       {isShowAlert && (
+      {isShowAlert && (
         <Alert
           variant="positive"
           isOpen={isShowAlert}
@@ -126,11 +128,9 @@ const Drawing_room = observer(() => {
           <p>Товар добавлен в корзину!</p>
         </Alert>
       )}
-     </>
-      
-    );
-  });
-  
-  export default Drawing_room;
+    </>
 
-  
+  );
+});
+
+export default DrawingRoom
