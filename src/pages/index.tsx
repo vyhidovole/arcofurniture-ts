c21e75caa8af7853e458489357d5309572f93c0d
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-07-03 10:16:46 +0300

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home = () => (
   <>
   <Head>
     <title>Arcofurniture</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
