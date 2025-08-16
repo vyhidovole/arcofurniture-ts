0552903a62e6ed1117c5c79e982ad2b04f684082
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-08-16 13:23:58 +0300

    lines corrected

--- src/components/ui/Product/Product.tsx
+++ src/components/ui/Product/Product.tsx
@@ -1,42 +1,42 @@
 import React from "react";
 import { VscTrash } from "react-icons/vsc";
 import { observer } from "mobx-react-lite"; // Импортируем observer
-import Image from "next/image"; 
-import { useCart } from '@/context/CartContext'; 
+import Image from "next/image";
+import { useCart } from '@/context/CartContext';
 import { ProductItem } from '@/types/types';
 import styles from './Product.module.css'
 
 export interface ProductProps {
   item: ProductItem;
- 
+
 }
 
 
 const Product: React.FC<ProductProps> = observer(({ item }) => {
   const cartStore = useCart(); // Используем контекст
 
-  
 
-  const { name, category, color,  imgSrc, id } = item;
+
+  const { name, category, color, imgSrc, id } = item;
   const productInBasket = cartStore.basket.find(p => p.id === item.id && item.category === p.category);
-const quantity = productInBasket?.quantity ?? 0;
+  const quantity = productInBasket?.quantity ?? 0;
 
-  
+
   const handleIncrement = () => {
     cartStore.incrementProductQuantity(item.id, item.category); // Увеличиваем количество в store
-    
+
   };
 
   const handleDecrement = () => {
     if (quantity > 1) {
       cartStore.decrementProductQuantity(item.id, item.category); // Уменьшаем количество в store
-      
+
     }
   };
 
   const handleDeleteProduct = () => {
-    cartStore.deleteProductFromBasket(item.id.toString(),item.category); // Вызываем deleteProduct из контекста
-    cartStore.clearProduct(id.toString(),item.category); // Также вызываем метод из store для удаления товара
+    cartStore.deleteProductFromBasket(item.id.toString(), item.category); // Вызываем deleteProduct из контекста
+    cartStore.clearProduct(id.toString(), item.category); // Также вызываем метод из store для удаления товара
   };
 
   const numericPrice = parseFloat(item.price); // Преобразуем в число
@@ -46,7 +46,12 @@ const quantity = productInBasket?.quantity ?? 0;
 
   return (
     <div className={styles["product-container"]}> {/* Основной контейнер */}
-      <Image width="100"height='100' src={imgSrc} alt={name} className={styles["product-image"]} /> {/* Изображение */}
+      <Image
+        src={imgSrc}
+        alt={name}
+        width="100"
+        height='100'
+        className={styles["product-image"]} /> {/* Изображение */}
       <div className={styles["product-info"]}> {/* Информация о продукте */}
         <h2 className={styles["product-name"]}>{name}</h2>
         <p className={styles["product-category"]}>Категория: {category}</p>
@@ -83,7 +88,7 @@ const quantity = productInBasket?.quantity ?? 0;
             +
           </button>
         </div>
-        <button 
+        <button
           className={styles["button-delete"]}
           onClick={handleDeleteProduct}
         >
