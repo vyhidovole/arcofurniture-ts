4f7f7b121bce49488c69bc53ede7010e684894dd
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-08-26 10:42:46 +0300

    upgrade

--- src/pages/account-page.tsx
+++ src/pages/account-page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Alert from "@/components/ui/Alert/Alert";
 import Input from "@/components/ui/Input/Input";
 import Button from "@/components/ui/Button/Button";
+import {useTheme} from '@/context/ThemeContext'
+import clsx from "clsx";
 import styles from "./account-page.module.css"
 
 /**
@@ -23,24 +25,24 @@ import styles from "./account-page.module.css"
  */
 // Определите тип InitialState, если он еще не определен
 interface InitialState {
-  name: string;
-  email: string;
-  password: string;
+    name: string;
+    email: string;
+    password: string;
 }
 interface AccountProps {
     setNewState: (data: InitialState) => void
 }
 
-const Account:React.FC<AccountProps> = ({ setNewState}) => {
-    
+const Account: React.FC<AccountProps> = ({ setNewState }) => {
+const {isDarkMode}= useTheme()
     const { formData, errors, handleChange, handleSubmit, resetForm } = useForm(
         { name: '', phone: '', email: "", password: '', confirmation: '' }, setNewState);
-   
+
     const [isShowAlert, setShowAlert] = useState(false);
     const [alertMessage, setAlertMessage] = useState('');
-    const [alertVariant, setAlertVariant] = useState<'positive' | 'negative'| 'info'>('info');
+    const [alertVariant, setAlertVariant] = useState<'positive' | 'negative' | 'info'>('info');
     const [isLoading, setIsLoading] = useState(false); // Состояние загрузки
-    const handleFormSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault(); // Предотвращаем перезагрузку страницы
         setIsLoading(true); // Устанавливаем состояние загрузки
 
@@ -48,8 +50,8 @@ const Account:React.FC<AccountProps> = ({ setNewState}) => {
         const isSuccess = await handleSubmit(e); // Предполагается, что handleSubmit возвращает true/false
 
         // Если форма успешно отправлена и нет ошибок
-      
-          if(isSuccess)  {
+
+        if (isSuccess) {
             localStorage.setItem('userData', JSON.stringify(formData));
 
             setShowAlert(true);
@@ -59,14 +61,14 @@ const Account:React.FC<AccountProps> = ({ setNewState}) => {
             }, 3000); // Закрываем алерт через 3 секунды
         } else {
             // Устанавливаем сообщение и показываем Alert
-        setAlertMessage("Данные введены не корректно.");
-        setAlertVariant('negative'); // Установите нужный вариант
-        setShowAlert(true);
-        setTimeout(() => {
-            setShowAlert(false)
-        }, 3000)
-        setIsLoading(false); // Сбрасываем состояние загрузки
-        return
+            setAlertMessage("Данные введены не корректно.");
+            setAlertVariant('negative'); // Установите нужный вариант
+            setShowAlert(true);
+            setTimeout(() => {
+                setShowAlert(false)
+            }, 3000)
+            setIsLoading(false); // Сбрасываем состояние загрузки
+            return
         }
         // Устанавливаем сообщение и показываем Alert
         setAlertMessage("Регисрация прошла успешно.");
@@ -83,92 +85,90 @@ const Account:React.FC<AccountProps> = ({ setNewState}) => {
     };
 
 
-   return (
-    <>
-        <h2 className={styles.title}>Быстрая регистрация</h2>
-        <div className={styles.linkContainer}>
-            <Link href="/" className={styles.link}>Главная</Link>
-            -
-            <p className={styles.text}>Быстрая регистрация</p>
-        </div>
-        <Alert
-            isOpen={isShowAlert}
-            onClose={handleCloseAlert}
-            variant={alertVariant}
-        >
-            {alertMessage}
-        </Alert>
-        <form
-            onSubmit={handleFormSubmit}
-            method="dialog"
-            className={styles.form}>
-            <div className={styles.inputContainer}>
-                <Input
-                    className={errors.name ? styles.inputError : ""}
-                    label="Name"
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    error={errors.name}
-                />
+    return (
+        <>
+            <h2 className={styles.title}>Быстрая регистрация</h2>
+            <div className={styles.linkContainer}>
+                <Link href="/" className={styles.link}>Главная</Link>
+                -
+                <p className={styles.text}>Быстрая регистрация</p>
             </div>
-            <div className={styles.inputContainer}>
-                <Input
-                    className={errors.phone ? styles.inputError : ""}
-                    label="Телефон"
-                    type="tel"
-                    name="phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    error={errors.phone}
-                />
-            </div>
-            <div className={styles.inputContainer}>
-                <Input
-                    className={errors.email ? styles.inputError : ""}
-                    label="Email"
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    error={errors.email}
-                />
-            </div>
-            <div className={styles.inputContainer}>
-                <Input
-                    className={errors.password ? styles.inputError : ""}
-                    label="Password"
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    error={errors.password}
-                />
-            </div>
-            <div className={styles.inputContainer}>
-                <Input
-                    className={errors.confirmation ? styles.inputError : ""}
-                    label="Confirmation"
-                    type="password"
-                    name="confirmation"
-                    value={formData.confirmation}
-                    onChange={handleChange}
-                    error={errors.confirmation}
-                />
-            </div>
-
-            <Button 
-                type="submit" 
-                variant="secondary"
-                isLoading={isLoading}
-                className={styles.button}
+            <Alert
+                isOpen={isShowAlert}
+                onClose={handleCloseAlert}
+                variant={alertVariant}
             >
-                Отправить
-            </Button>
-        </form>
-    </>
-);
+                {alertMessage}
+            </Alert>
+            <form
+                onSubmit={handleFormSubmit}
+                method="dialog"
+                className={`${styles.form} ${isDarkMode?'bg-dark':'bg-light'}`}>
+                <div className={clsx(styles.inputContainer, { [styles.inputError]: errors.name })}>
+                    <Input
+                        label="Имя"
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        error={errors.name}
+                    />
+                </div>
+                <div className={clsx(styles.inputContainer, { [styles.inputError]: errors.phone })}>
+                    <Input
+                       label="Телефон"
+                        type="tel"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        error={errors.phone}
+                    />
+                </div>
+               <div className={clsx(styles.inputContainer, { [styles.inputError]: errors.email})}>
+                    <Input
+                        
+                        label="Email"
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        error={errors.email}
+                    />
+                </div>
+                <div className={clsx(styles.inputContainer, { [styles.inputError]: errors.password})}>
+                    <Input
+                        
+label="Пароль"
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        error={errors.password}
+                    />
+                </div>
+                <div className={clsx(styles.inputContainer, { [styles.inputError]: errors.confirmation})}>
+                    <Input
+                        
+                        label="Подтверждение"
+                        type="password"
+                        name="confirmation"
+                        value={formData.confirmation}
+                        onChange={handleChange}
+                        error={errors.confirmation}
+                    />
+                </div>
+
+                <Button
+                    type="submit"
+                    variant="secondary"
+                    isLoading={isLoading}
+                    className={styles.button}
+                >
+                    Отправить
+                </Button>
+            </form>
+        </>
+    );
 }
 
 export default Account;
