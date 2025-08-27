1a52c3f37776f50274a4f51e731c92e6758d6d7e
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-08-27 10:37:25 +0300

    corrected

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 // server.js
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Для получения __dirname в ES6 модулях
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
