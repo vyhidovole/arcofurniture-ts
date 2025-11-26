f2464d695a9084978d0504501e399392fe846f05
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-11-26 09:40:00 +0300

    commented

--- src/hooks/useValidate.ts
+++ src/hooks/useValidate.ts
@@ -1,77 +1,79 @@
-import { useState } from "react";
-import { validateForm } from "../utils/validators";
+// В этом проекте не применяется. В место него useForm.
 
-interface InitialState {
-    [key:string]:string
-}
+// import { useState } from "react";
+// import { validateForm } from "../utils/validators";
 
-interface Errors {
-    [key:string]:string|undefined
-}
+// interface InitialState {
+//     [key:string]:string
+// }
 
-interface UseValidateReturn {
-    formState:InitialState
-    errors:Errors
-    handleInput:(e:React.ChangeEvent<HTMLInputElement>) => void
-}
+// interface Errors {
+//     [key:string]:string|undefined
+// }
 
-/**
- * Хук для управления состоянием формы и валидацией.
- *
- * @param {initialState} initialState - Начальное состояние формы.
- * @returns {UseValidateReturn} - Объект с состоянием формы, ошибками и функциями.
- */
-function useValidate(initialState:InitialState):UseValidateReturn {
-  // Состояние формы, хранит значения полей
-  const [formState, setFormState] = useState<InitialState>(initialState);
+// interface UseValidateReturn {
+//     formState:InitialState
+//     errors:Errors
+//     handleInput:(e:React.ChangeEvent<HTMLInputElement>) => void
+// }
 
-  // Состояние для отслеживания ошибок валидации
-  const [errors, setErrors] = useState<Errors>({});
+// /**
+//  * Хук для управления состоянием формы и валидацией.
+//  *
+//  * @param {initialState} initialState - Начальное состояние формы.
+//  * @returns {UseValidateReturn} - Объект с состоянием формы, ошибками и функциями.
+//  */
+// function useValidate(initialState:InitialState):UseValidateReturn {
+//   // Состояние формы, хранит значения полей
+//   const [formState, setFormState] = useState<InitialState>(initialState);
 
-  // Состояние для отслеживания полей, в которых пользователь находился
-  // const [touchedFields, setTouchedFields] = useState({});
+//   // Состояние для отслеживания ошибок валидации
+//   const [errors, setErrors] = useState<Errors>({});
 
-  /**
-   * Обработчик изменения значения полей формы.
-   *
-   * @param {Object} e - Событие изменения.
-   */
-  const handleInput = (e:React.ChangeEvent<HTMLInputElement> ) => {
-    const { name, value } = e.target;
+//   // Состояние для отслеживания полей, в которых пользователь находился
+//   // const [touchedFields, setTouchedFields] = useState({});
 
-    // Обновляем состояние формы для текущего поля
-    const updatedFormState = { ...formState, [name]: value };
-    setFormState(updatedFormState);
+//   /**
+//    * Обработчик изменения значения полей формы.
+//    *
+//    * @param {Object} e - Событие изменения.
+//    */
+//   const handleInput = (e:React.ChangeEvent<HTMLInputElement> ) => {
+//     const { name, value } = e.target;
 
-    // Валидируем только текущее поле
-    const validationErrors = {
-      ...errors,
-      [name]: validateForm({ [name]: value })[name],
+//     // Обновляем состояние формы для текущего поля
+//     const updatedFormState = { ...formState, [name]: value };
+//     setFormState(updatedFormState);
+
+//     // Валидируем только текущее поле
+//     const validationErrors = {
+//       ...errors,
+//       [name]: validateForm({ [name]: value })[name],
       
-// 1. Создается временный объект { [name]: value }, где [name] — это динамическое имя свойства,
-//    то есть название поля формы, которое сейчас валидируем.
-// 2. Передается этот объект в функцию validateForm, которая возвращает объект ошибок типа ValidationResult.
-// 3. После вызова validateForm, мы сразу обращаемся к свойству этого объекта по ключу [name], чтобы получить сообщение об ошибке или undefined, если ошибки нет.
-// 4. В итоге, мы присваиваем это сообщение свойству [name] в объекте validationErrors, чтобы обновить ошибку для этого конкретного поля без трогания других полей.
-    };
+// // 1. Создается временный объект { [name]: value }, где [name] — это динамическое имя свойства,
+// //    то есть название поля формы, которое сейчас валидируем.
+// // 2. Передается этот объект в функцию validateForm, которая возвращает объект ошибок типа ValidationResult.
+// // 3. После вызова validateForm, мы сразу обращаемся к свойству этого объекта по ключу [name], чтобы получить сообщение об ошибке или undefined, если ошибки нет.
+// // 4. В итоге, мы присваиваем это сообщение свойству [name] в объекте validationErrors, чтобы обновить ошибку для этого конкретного поля без трогания других полей.
+//     };
 
-    // Обновляем состояние ошибок
-    setErrors(validationErrors);
-  };
+//     // Обновляем состояние ошибок
+//     setErrors(validationErrors);
+//   };
 
-  // Внутренний обработчик фокуса
-  // const handleBlur = (e) => {
-  //   const { name } = e.target;
+//   // Внутренний обработчик фокуса
+//   // const handleBlur = (e) => {
+//   //   const { name } = e.target;
 
-  //   // Устанавливаем касаемость поля внутри хука
-  //   setTouchedFields((prevTouched) => ({ ...prevTouched, [name]: true }));
-  // };
+//   //   // Устанавливаем касаемость поля внутри хука
+//   //   setTouchedFields((prevTouched) => ({ ...prevTouched, [name]: true }));
+//   // };
 
-  return {
-    formState,
-    errors,
-    handleInput,
-  };
-}
+//   return {
+//     formState,
+//     errors,
+//     handleInput,
+//   };
+// }
 
-export default useValidate;
+// export default useValidate;
