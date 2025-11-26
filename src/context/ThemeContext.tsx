ab33740f18eeb2016b1b5ca048900323778c7153
Oleg Vakhidov <vyhidovoleg@gmail.com>
2025-11-26 09:38:16 +0300

    comment deleted

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,65 +1,4 @@
 
-// import React, { createContext, useContext, useState, ReactNode } from 'react';
-// /**
-//  * Тип данных для контекста темы.
-//  * @property {boolean} isDarkMode - Текущий режим темы: dark или light.
-//  * @property {() => void} toggleTheme - Функция для переключения темы.
-//  */
-// type ThemeContextType = {
-//   isDarkMode: boolean;
-//   toggleTheme: () => void;
-// };
-// /**
-//  * Создаем контекст темы.
-//  */
-// const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
-// /**
-//  * Свойства компонента ThemeProvider.
-//  * @property {ReactNode} children - Дочерние компоненты.
-//  */
-// type ThemeProviderProps = {
-//   children: ReactNode;
-// };
-// /**
-//  * Провайдер контекста темы.
-//  * Оборачивает дочерние компоненты и управляет состоянием темы.
-//  * @param {ThemeProviderProps} props - Свойства компонента.
-//  * @returns {JSX.Element} Компонент провайдера.
-//  */
-// export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-//   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
-// /**
-//    * Переключает режим темы между светлой и темной.
-//    */
-//   const toggleTheme = () => {
-//     setIsDarkMode((prevMode) => !prevMode);
-//   };
-//  /**
-//    * Значение для контекста.
-//    */
-//   const value: ThemeContextType = {
-//     isDarkMode,
-//     toggleTheme,
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={value}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
-// /**
-//  * Хук для использования контекста темы.
-//  * @returns {ThemeContextType} Объект с текущим состоянием темы и методом переключения.
-//  * @throws {Error} Если вызван вне компонента с ThemeProvider.
-//  */
-// export const useTheme = (): ThemeContextType => {
-//   const context = useContext(ThemeContext);
-//   if (!context) {
-//     throw new Error('useTheme must be used within a ThemeProvider');
-//   }
-//   return context;
-// };
 import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
 
 // Определяем интерфейс для контекста
@@ -112,7 +51,11 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   );
 };
 
-// Хук для использования контекста
+/**
+  * Хук для использования контекста темы.
+  * @returns {ThemeContextType} Объект с текущим состоянием темы и методом переключения.
+  * @throws {Error} Если вызван вне компонента с ThemeProvider.
+ */
 export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (context === undefined) {
